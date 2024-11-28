417921f34494be7dd75a331a4f9620ba3bd5ba91
RainStorMtj <145570462+RainStorMtj@users.noreply.github.com>
2024-11-28 23:42:06 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -3,6 +3,9 @@ def print_params(a = 1, b = 'Строка', c = True):
 values_list = ['Орёл', 23, 4.6]
 values_dict = {'a': 9, 'b': 'Решка', 'c': False}
 values_list_2 = ['Орешка', 2.4]
+print_params(a = 'Привет', c = 472)
+print_params(b = 93, a = 'Пока')
+print_params()
 print_params(b = 25)
 print_params(c = [1, 2, 3])
 print_params(*values_list)
